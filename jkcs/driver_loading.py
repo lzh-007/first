57afc1923ce9686a158e73a8faed9abbba5ad4f6
lzh <1149831246@qq.com>
2024-03-18 19:42:06 +0800

    1

---
+++ jkcs/driver_loading.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as  ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+#全自动安装驱动
+driver=webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://www.baidu.com")
---
+++ jkcs/test_code.py
@@ -0,0 +1,24 @@
+import xToolkit
+import requests
+import pytest
+import os
+import allure_pytest
+
+
+all_Case=xToolkit.xfile.read("test.xls").excel_to_dict(sheet=0)
+print("所有信息：",all_Case)
+
+
+@pytest.mark.parametrize("case",all_Case)
+def test_api(case):
+    response=requests.request(url=eval(case["接口URL"]),
+                              method=case["请求方式"],
+                              json=eval(case["URL参数"]) ,
+                              headers=eval(case["参数类型"]) )
+
+    assert response.status_code==case["预期状态码"],"测试不通过，状态码错误"
+
+
+if __name__ == '__main__':
+    pytest.main(['--alluredir=allure-results'])
+    os.system(r"allure generate -c -o allure-report")
